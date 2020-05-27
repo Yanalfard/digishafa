6860c7effaa5272c7fc3d72f8ef2585fb5193ae9
yanal <yanal.fard@gmail.com>
2020-05-27 11:01:10 +0430

    ConnectionStringAdded

--- DigiShafa/Utilities/Config.cs
+++ DigiShafa/Utilities/Config.cs
@@ -2,6 +2,6 @@ namespace DigiShafa.Utilities
 {
     public static class Config
     {
-        public static readonly string ConnectionString = "#ConnectionString#";
+        public static readonly string ConnectionString = "Data Source=109.169.76.79;Initial Catalog=arvintav_DigiShafa;User ID=arvintav_Root;Password=1710ahmad.fard";
     }
 }
